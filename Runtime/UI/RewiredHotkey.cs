1218ef850dabdd521bef6ef78a90e62eb7b5007c
p.shestakov <0xShestakov@gmail.com>
2024-05-20 17:18:51 +0300

    temporary removed ActionIdProperty from RewiredHotkey, changes in package.json

--- Runtime/UI/RewiredHotkey.cs
+++ Runtime/UI/RewiredHotkey.cs
@@ -127,9 +127,6 @@ namespace Rewired.UI.Hotkeys
         public class HotkeyAction
         {
             [SerializeField]
-#if REWIRED_UI_HOTKEYS_USE_CONSTS
-            [ActionIdProperty(typeof(RewiredConsts.Action))]
-#endif
             private int _actionId = -1;
             public int actionId
             {
