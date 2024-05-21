6e5e4e9d04097514b0227e64f1041e1b038f20c5
p.shestakov <0xShestakov@gmail.com>
2024-05-21 12:10:17 +0300

    added ability to create Rewired Hotkeys Manger via Unity Editor's menu

---
+++ Editor/RewiredEditorUtility.cs
@@ -0,0 +1,99 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Rewired.UI.Hotkeys
+{
+    public static class RewiredEditorUtility
+    {
+        private const string _objectName = "Rewired Hotkeys Manager";
+        private static readonly string _objectNameAndExtension = $"{_objectName}.prefab";
+        private static readonly string _assetPath = $"Assets/Rewired.UI.Hotkeys/{_objectNameAndExtension}";
+
+        [MenuItem("Window/Rewired/Create/Hotkeys Manager (in scene)")]
+        public static void CreateObjectInScene()
+        {
+            var existedObject = FindObjectInScene();
+            if (existedObject != null)
+            {
+                EditorUtility.DisplayDialog("Warning", $"{_objectNameAndExtension} already exists in scene", "Okay");
+                Selection.activeObject = existedObject;
+                return;
+            }
+            CreateObject();
+
+            GameObject FindObjectInScene()
+            {
+                var hotkeys = Object.FindObjectOfType<RewiredHotkeys>();
+                if (hotkeys != null)
+                    return hotkeys.gameObject;
+
+                var glyphs = Object.FindObjectOfType<RewiredGlyphs>();
+                if (glyphs != null)
+                    return glyphs.gameObject;
+
+                var activeScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+                var rootObjects = activeScene.GetRootGameObjects();
+                foreach (var obj in rootObjects)
+                {
+                    if (obj.name == _objectName)
+                        return obj;
+                }
+
+                return null;
+            }
+        }
+
+        [MenuItem("Window/Rewired/Create/Hotkeys Manager (prefab)")]
+        public static void CreateObjectAsPrefab()
+        {
+            var existedPrefab = AssetDatabase.LoadAssetAtPath<GameObject>(_assetPath);
+            if (existedPrefab != null)
+            {
+                EditorUtility.DisplayDialog("Warning", $"{_objectNameAndExtension} already exists in project at {_assetPath}", "Okay");
+                Selection.activeObject = existedPrefab;
+                return;
+            }
+
+            var assetFolder = Path.GetDirectoryName(_assetPath).Replace("Assets\\", "");
+            AssetDatabase.CreateFolder("Assets", assetFolder);
+
+            var obj = CreateObject();
+            try
+            {
+                var prefab = PrefabUtility.SaveAsPrefabAsset(obj, _assetPath, out var success);
+                AssetDatabase.Refresh();
+                if (success)
+                    Selection.activeObject = prefab;
+            }
+            finally
+            {
+                Object.DestroyImmediate(obj.gameObject);
+            }
+        }
+
+        private static GameObject CreateObject()
+        {
+            GameObject obj = new GameObject(_objectName);
+            var hotkeys = obj.AddComponent<RewiredHotkeys>();
+            var glyphs = obj.AddComponent<RewiredGlyphs>();
+
+            var profiles = FindProfilesInAssets();
+            glyphs.EditorSetProfiles(profiles);
+            return obj;
+        }
+
+        public static RewiredControllerProfile[] FindProfilesInAssets()
+        {
+            var profilesGuids = AssetDatabase.FindAssets($"t:{typeof(RewiredControllerProfile)}");
+            var profiles = new RewiredControllerProfile[profilesGuids.Length];
+            for (int i = 0; i < profiles.Length; ++i)
+            {
+                var guid = profilesGuids[i];
+                var path = AssetDatabase.GUIDToAssetPath(guid);
+                profiles[i] = AssetDatabase.LoadAssetAtPath<RewiredControllerProfile>(path);
+            }
+            return profiles;
+        }
+    }
+}
--- Editor/RewiredGlyphsInspector.cs
+++ Editor/RewiredGlyphsInspector.cs
@@ -13,14 +13,7 @@ namespace Rewired.UI.Hotkeys
             var glyphs = target as RewiredGlyphs;
             if (GUILayout.Button("Find Profiles"))
             {
-                var profilesGuids = AssetDatabase.FindAssets($"t:{typeof(RewiredControllerProfile)}");
-                var profiles = new RewiredControllerProfile[profilesGuids.Length];
-                for (int i =0; i < profiles.Length; ++i)
-                {
-                    var guid = profilesGuids[i];
-                    var path = AssetDatabase.GUIDToAssetPath(guid);
-                    profiles[i] = AssetDatabase.LoadAssetAtPath<RewiredControllerProfile>(path);
-                }
+                var profiles = RewiredEditorUtility.FindProfilesInAssets();
                 glyphs.EditorSetProfiles(profiles);
                 EditorUtility.SetDirty(target);
             }
